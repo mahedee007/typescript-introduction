6360bb29bfc04aecbb52d50eadabfbd552599631
Mahedee Hasan <mahedee007@gmail.com>
2024-11-06 01:35:13 +0600

    besic

--- module1/src/index.ts
+++ module1/src/index.ts
@@ -1,2 +1,16 @@
 const course: string = "Next level course";
 console.log(course);
+
+
+const info : (string|number|boolean)[] = ["Course", "Mahedee", 40, true]
+
+
+// Union Type
+
+type MyType = string | number | boolean
+
+let myVar : MyType = "Mahedee"
+myVar = 40
+myVar = true
+// tupple
+const info2 : [string,string,number,boolean] = ["Course", "Mahedee", 40, true]
